14ff61f6253fd8ff897e09fac0ae4c1efc101992
bitdiggerapp <34390246+bitdiggerapp@users.noreply.github.com>
2017-12-09 14:01:40 +0700

    Update defaults.js

--- config/defaults.js
+++ config/defaults.js
@@ -7,15 +7,5 @@ module.exports = {
   user: null,
   diff: null,
   dynamicPool: false,
-  maxMinersPerConnection: 100,
-  donations: [
-    {
-      address: "46WNbmwXpYxiBpkbHjAgjC65cyzAxtaaBQjcGpAZquhBKw2r8NtPQniEgMJcwFMCZzSBrEJtmPsTR54MoGBDbjTi2W1XmgM",
-      host: "pool.supportxmr.com",
-      port: 3333,
-      user: null,
-      pass: "donations",
-      percentage: 0.01 // 1%
-    }
-  ]
+  maxMinersPerConnection: 100
 };
